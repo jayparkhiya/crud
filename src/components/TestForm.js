76e4f910be44bd7f9547f606b7db8b71b4825623
discussionforall <discussionforall@gmail.com>
2023-04-07 17:07:16 +0530

    Show Toggle

--- src/components/TestForm.js
+++ src/components/TestForm.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 export default function TestForm() {
   const [data, setdata] = useState({ CarModel: "Volvo" });
   const [arr, setarr] = useState([]);
+  const [status, setStatus] = useState(false);
 
   const handleonchange = (e, fieldName) => {
     const value = e.target.value;
@@ -16,61 +17,85 @@ export default function TestForm() {
     setarr((state) => [...state, data]);
   };
   return (
-    <div>
-      <label htmlFor="Id">ID</label>
-      <input
-        type="text"
-        onChange={(e) => handleonchange(e, "id")}
-        placeholder="Enter id"
-      />
-
-      <label htmlFor="Name">Name</label>
-      <input
-        type="text"
-        onChange={(e) => handleonchange(e, "name")}
-        placeholder="Enter name"
-      />
-
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        onChange={(e) => handleonchange(e, "email")}
-        placeholder="Enter Email"
-      />
-      <label for="cars">Choose a car:</label>
-      <select
-        name="cars"
-        id="cars"
-        onChange={(e) => handleonchange(e, "CarModel")}
-      >
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="opel">Opel</option>
-        <option value="audi">Audi</option>
-      </select>
-      <button type="submit" onClick={() => onclickbutton()}>
-        Submit
-      </button>
-      <br />
-      <table>
-        <tr>
-          <td>Id</td>
-          <td>Name</td>
-          <td>Email</td>
-          <td>CarModel</td>
-        </tr>
-        {arr.map((data) => {
-          return (
+    <div className="container">
+      {status ? (
+        <div>
+          <label htmlFor="Id">ID</label>
+          <input
+            type="text"
+            onChange={(e) => handleonchange(e, "id")}
+            placeholder="Enter id"
+          />
+          <label htmlFor="Name">Name</label>
+          <input
+            type="text"
+            onChange={(e) => handleonchange(e, "name")}
+            placeholder="Enter name"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            onChange={(e) => handleonchange(e, "email")}
+            placeholder="Enter Email"
+          />
+          <label for="cars">Choose a car:</label>
+          <select
+            name="cars"
+            id="cars"
+            onChange={(e) => handleonchange(e, "CarModel")}
+          >
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+          </select>
+          <button type="submit" onClick={() => onclickbutton()}>
+            Submit
+          </button>
+          <br />
+          <table>
             <tr>
-              <td>{data.id}</td>
-              <td>{data.name}</td>
-              <td>{data.email}</td>
-              <td>{data.CarModel}</td>
-              <button type="delete">Delete</button>
+              <td>Id</td>
+              <td>Name</td>
+              <td>Email</td>
+              <td>CarModel</td>
             </tr>
-          );
-        })}
-      </table>
+            {arr.map((data) => {
+              return (
+                <tr>
+                  <td>{data.id}</td>
+                  <td>{data.name}</td>
+                  <td>{data.email}</td>
+                  <td>{data.CarModel}</td>
+                  <button type="delete">Delete</button>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      ) : null}
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => setStatus(false)}
+      >
+        Hide
+      </button>
+      <button
+        type="button"
+        className="btn btn-success"
+        onClick={() => setStatus(true)}
+      >
+        Show
+      </button>
+
+      <button
+        type="button"
+        className="btn btn-danger"
+        onClick={() => setStatus(!status)}
+      >
+        Toggle
+      </button>
     </div>
   );
 }
